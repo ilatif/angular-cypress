04495650da43a6f0f41c56ec00538dcdae58205f
Imran Latif <ilatif.bwp@gmail.com>
2021-02-11 15:40:22 +0500

    Remote baseUrl and add url to the live site manually

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,7 +2,7 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl);
+    return browser.get('https://betteroffcalculator.co.uk/');
   }
 
   async getTitleText(): Promise<string> {
