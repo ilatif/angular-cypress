b0b5d0dc49586f3114d591528e4eb3503704543e
Imran Latif <ilatif.bwp@gmail.com>
2021-03-04 20:42:03 +0500

    Add a test route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
