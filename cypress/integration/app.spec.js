d1b76788a580234a71a3dce4569d7c84e5bb1308
Imran Latif <ilatif.bwp@gmail.com>
2021-05-06 00:47:50 +0500

    Update spec to test cache

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,6 +2,6 @@ describe("First test in Cypress", () => {
   it('visit app', () => {
     cy.visit('/');
     cy.get('.app-title').contains('angular-cypress app is running');
-    cy.get('.terminal').contains('ng generate component xy');
+    cy.get('.terminal').contains('ng generate component xyz');
   });
 });
