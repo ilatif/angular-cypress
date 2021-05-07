ae37fec04a2b8346d7fa9e1d3328324b86320e9f
Imran Latif <ilatif.bwp@gmail.com>
2021-05-07 17:44:42 +0500

    Fix failing test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,6 +2,6 @@ describe("First test in Cypress", () => {
   it('visit app', () => {
     cy.visit('');
     cy.get('.app-title').contains('angular-cypress app is running');
-    cy.get('.terminal').contains('ng generate component im');
+    cy.get('.terminal').contains('ng generate component xy');
   });
 });
