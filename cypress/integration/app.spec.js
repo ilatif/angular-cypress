8d706cbdb8133474828a2e3783437730fa552d9a
Imran Latif <ilatif.bwp@gmail.com>
2021-05-07 17:08:16 +0500

    Trying getting current username dynamically

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,7 @@
 describe("First test in Cypress", () => {
   it('visit app', () => {
-    cy.visit('/');
+    cy.visit('');
     cy.get('.app-title').contains('angular-cypress app is running');
-    cy.get('.terminal').contains('ng generate component xyz');
+    cy.get('.terminal').contains('ng generate component im');
   });
 });
