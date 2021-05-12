214b9e014f614eb4e663d02f4cb15f5d7130498c
Imran Latif <ilatif.bwp@gmail.com>
2021-05-12 16:56:10 +0500

    Try reproducing tests failing issues on Windows

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,7 @@
-// describe("First test in Cypress", () => {
-//   it('visit app', () => {
-//     cy.visit('');
-//     cy.get('.app-title').contains('angular-cypress app is running');
-//     cy.get('.terminal').contains('ng generate component xy');
-//   });
-// });
+describe("First test in Cypress", () => {
+  it('visit app', () => {
+    cy.visit('/');
+    // cy.get('.app-title').contains('angular-cypress app is running');
+    // cy.get('.terminal').contains('ng generate component xy');
+  });
+});
